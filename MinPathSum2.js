12a81c9a4d7f05b7c427f9c44e02da02ded1e7b3
bfitz22 <bffitzgerald89@gmail.com>
2019-06-21 21:38:58 -0400

    refactor

--- MinPathSum2.js
+++ MinPathSum2.js
@@ -1,23 +1,4 @@
-var minPathSum = function(grid, row = 0, col = 0) {
-    const numRows = grid.length - 1;
-    const numCols = grid[0].length - 1;
- 
-    if (row === numRows && col === numCols) {
-        return grid[numRows][numCols];
-    }
- 
-    if (row > numRows || col > numCols) return Infinity;
- 
-    let currNum = grid[row][col];
-    let bottom = minPathSum(grid, row + 1, col);
-    let right = minPathSum(grid, row, col + 1);
- 
-    if (bottom > right) { return currNum + right };
-    if (right >= bottom) { return currNum + bottom };
- 
- };
-
- var minPathSum = function (grid, row = 0, col = 0, memo = {}) {
+var minPathSum = function (grid, row = 0, col = 0, memo = {}) {
     const numRows = grid.length - 1;
     const numCols = grid[0].length - 1;
  
