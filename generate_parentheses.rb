9d3fbbcef45167835a3dcc2e7a328295ac74ca68
bfitz22 <bffitzgerald89@gmail.com>
2019-09-16 23:53:11 -0400

    solve 'generate_parentheses'

---
+++ generate_parentheses.rb
@@ -0,0 +1,35 @@
+def generate_parentheses(n)
+    combinations = []
+    current = []
+    (n * 2).times { current << "" }
+    generate_all(current, 0, combinations)
+    return combinations
+end
+
+def generate_all(current, pos, result)
+    if pos == current.length
+        if valid(current)
+            result << current.to_s
+        end
+    else
+        current[pos] = "("
+        generate_all(current, pos + 1, result)
+        current[pos] = ")"
+        generate_all(current, pos + 1, result)
+    end
+end
+
+def valid(current) 
+    balance = 0
+    current.each do |char|
+        if char == "("
+            balance += 1
+        else
+            balance -= 1
+        end
+        return false if balance < 0
+    end
+    return balance == 0
+end
+
+puts generate_parentheses(4)
